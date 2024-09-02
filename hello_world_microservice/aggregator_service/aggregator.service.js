d3426fd5246945581ae1fcd8ef588df5996a33bc
Rajat Mishra <rajatmishra343@gmail.com>
2024-09-01 18:58:35 -0700

    aggregator service fixed for docker networl

--- hello_world_microservice/aggregator_service/aggregator.service.js
+++ hello_world_microservice/aggregator_service/aggregator.service.js
@@ -5,8 +5,44 @@ const app = express();
 
 app.get("/helloworld", async (req, res) => {
 	try {
-		const helloResponse = await axios.get("http://localhost:3001/hello");
-		const worldResponse = await axios.get("http://localhost:3002/world");
+		let helloResponse;
+		let worldResponse;
+		try {
+			helloResponse = await axios.get("http://localhost:3001/hello");
+		} catch (err) {
+			console.error(
+				`Error getting data from hello server: ${err?.message}`
+			);
+		}
+		if (!helloResponse) {
+			try {
+				helloResponse = await axios.get(
+					"http://hello_container:3001/hello"
+				);
+			} catch (err) {
+				console.error(
+					`Error getting data from hello container: ${err?.message}`
+				);
+			}
+		}
+		try {
+			worldResponse = await axios.get("http://localhost:3002/world");
+		} catch (err) {
+			console.error(
+				`Error getting data from world server: ${err?.message}`
+			);
+		}
+		if (!worldResponse) {
+			try {
+				worldResponse = await axios.get(
+					"http://world_container:3002/world"
+				);
+			} catch (err) {
+				console.error(
+					`Error getting data from world container: ${err?.message}`
+				);
+			}
+		}
 		const combinedMessage = `${helloResponse.data} ${worldResponse.data}`;
 		res.send(combinedMessage);
 	} catch (error) {
