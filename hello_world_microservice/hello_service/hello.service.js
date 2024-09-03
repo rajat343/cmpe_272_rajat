c6812bf7c207eeaddb548317ab62cb3c33a8f46e
Rajat Mishra <rajatmishra343@gmail.com>
2024-09-03 02:19:33 -0700

    made caps

--- hello_world_microservice/hello_service/hello.service.js
+++ hello_world_microservice/hello_service/hello.service.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/hello", (req, res) => {
-	res.send("hello");
+	res.send("Hello");
 });
 
 app.listen(3001, () => {
--- hello_world_microservice/world_service/world.service.js
+++ hello_world_microservice/world_service/world.service.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/world", (req, res) => {
-	res.send("world");
+	res.send("World");
 });
 
 app.listen(3002, () => {
